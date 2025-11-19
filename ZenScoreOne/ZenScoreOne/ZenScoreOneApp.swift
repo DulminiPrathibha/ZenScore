56bcd63a3caaae3a7b2203a1a19809c66510626f
DulminiPrathibha <dulminiprathibha@gmail.com>
2025-11-19 12:08:37 +0530

    Log In Ui

---
+++ ZenScoreOne/ZenScoreOne/ZenScoreOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenScoreOneApp.swift
+//  ZenScoreOne
+//
+//  Created by Supuni Nethsarani on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct ZenScoreOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
